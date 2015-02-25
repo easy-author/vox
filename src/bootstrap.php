93059e670568bdd7c9c2cbebff9417cee5996fe4
Potfur <wachowski.michal@gmail.com>
2015-02-25 02:07:16 +0100

    include fix

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,7 @@ return array(
     'import' => array(
         (array) require __DIR__ . '/../src/Vox/bootstrap.php',
         (array) require __DIR__ . '/../src/Vox/bootstrap.db.php',
-        is_file($dbConnectionPath) ? (array) $dbConnectionPath : [],
+        is_file($dbConnectionPath) ? (array) require $dbConnectionPath : [],
 
         (array) require __DIR__ . '/../src/Console/bootstrap.php',
     ),
