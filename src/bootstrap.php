4739962801558054f9d3b0b45cb998858e8872b2
Potfur <wachowski.michal@gmail.com>
2015-02-25 02:03:19 +0100

    database con is conditional

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
+$dbConnectionPath = __DIR__ . '/../src/Vox/bootstrap.db.connection.php';
+
 return array(
     'import' => array(
         (array) require __DIR__ . '/../src/Vox/bootstrap.php',
         (array) require __DIR__ . '/../src/Vox/bootstrap.db.php',
-        (array) require __DIR__ . '/../src/Vox/bootstrap.db.connection.php',
+        is_file($dbConnectionPath) ? (array) $dbConnectionPath : [],
 
         (array) require __DIR__ . '/../src/Console/bootstrap.php',
     ),
